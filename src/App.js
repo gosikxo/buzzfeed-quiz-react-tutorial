823dc7884ba64d29423bf1587251d73e9c3ba2cd
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-01-06 18:13:16 +0100

    Fetch quiz data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react"
 import { Title } from "./components/Title"
 
 const App = () => {
+  const [quiz, setQuiz] = useState(false)
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/quiz')
+      const json = await response.json()
+      setQuiz(json)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <Title />
